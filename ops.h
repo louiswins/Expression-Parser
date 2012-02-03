987d992780d2ebbffcef6fbb685a920bf0a32a7c
Louis Wilson <louis.wilson@asu.edu>
2012-02-02 18:27:18 -0700

    Simplified class hierarchy

--- ops.h
+++ ops.h
@@ -5,33 +5,12 @@
 #include <tr1/memory>
 
 class ptree;
-/* Just use ptree_ptr instead of ptree* and you'll never have a memory leak!
- * (For certain definitions of "never".) You can even use it in an STL
- * container! What joy! */
 typedef std::tr1::shared_ptr<ptree> ptree_ptr;
 
 class ptree {
 public:
 	virtual int eval() = 0;
 	virtual void print(int indent=0) = 0;
-};
-
-class bin_operator : public ptree {
-public:
-	bin_operator(ptree_ptr left, ptree_ptr right) : l(left), r(right) {}
-	bin_operator(ptree *left, ptree *right) : l(left), r(right) {}
-
-protected:
-	ptree_ptr l;
-	ptree_ptr r;
-};
-class un_operator : public ptree {
-public:
-	un_operator(ptree_ptr term) : t(term) {}
-	un_operator(ptree *term) : t(term) {}
-
-protected:
-	ptree_ptr t;
 };
 
 class int_literal : public ptree {
@@ -47,26 +26,34 @@ protected:
 	int v;
 };
 
-class amp_operator : public bin_operator {
+class amp_operator : public ptree {
 public:
-	amp_operator(ptree_ptr l, ptree_ptr r) : bin_operator(l, r) {}
-	amp_operator(ptree *l, ptree *r) : bin_operator(l, r) {}
-	int eval();
-	void print(int indent);
-};
-class at_operator : public bin_operator {
-public:
-	at_operator(ptree_ptr l, ptree_ptr r) : bin_operator(l, r) {}
-	at_operator(ptree *l, ptree *r) : bin_operator(l, r) {}
+	amp_operator(ptree_ptr left, ptree_ptr right) : l(left), r(right) {}
+	amp_operator(ptree *left, ptree *right) : l(left), r(right) {}
 	int eval();
 	void print(int indent=0);
+private:
+	ptree_ptr l;
+	ptree_ptr r;
 };
-class pct_operator : public un_operator {
+class at_operator : public ptree {
 public:
-	pct_operator(ptree_ptr t) : un_operator(t) {}
-	pct_operator(ptree *t) : un_operator(t) {}
+	at_operator(ptree_ptr left, ptree_ptr right) : l(left), r(right) {}
+	at_operator(ptree *left, ptree *right) : l(left), r(right) {}
 	int eval();
 	void print(int indent=0);
+private:
+	ptree_ptr l;
+	ptree_ptr r;
+};
+class pct_operator : public ptree {
+public:
+	pct_operator(ptree_ptr tree) : t(tree) {}
+	pct_operator(ptree *tree) : t(tree) {}
+	int eval();
+	void print(int indent=0);
+private:
+	ptree_ptr t;
 };
 
 #endif /* OPS_H */
